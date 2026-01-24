585a50ce54134b63cad42260f6063194519d52e4
hayaf3254 <poso42731427@gmail.com>
2026-01-25 03:02:40 +0900

    フロント:CSSのファイル分離、ログイン後にログインスタンプ画面にいくように変更

--- apps/web/app/dashboard/page.tsx
+++ apps/web/app/dashboard/page.tsx
@@ -1,9 +1,5 @@
-{/* TODO: 後で削除 - 仮置きページ */}
+import { StampScreen } from "@/components/features/stamp/StampScreen";
 
 export default function DashboardPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <p>ログイン成功（仮）</p>
-    </div>
-  );
+  return <StampScreen />;
 }
